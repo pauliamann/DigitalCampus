9cc11c6b4519b7c1018dd22e453b956faac1a65e
paul <paul.amann@gmx.at>
2020-10-06 09:28:24 +0200

    Funktion: Sortierung längstes Wort zugefügt

--- DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w04/W04Tag1HesseFileEinlesenUndAuswerten.java
+++ DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w04/W04Tag1HesseFileEinlesenUndAuswerten.java
@@ -22,7 +22,9 @@ public class W04Tag1HesseFileEinlesenUndAuswerten {
             Scanner sc = new Scanner(fis);
 
             while (sc.hasNext()) {
+
                 text += sc.nextLine();
+                text += " " ;
                 //System.out.println(" File content : " + input1);
             }
             try {
@@ -37,13 +39,14 @@ public class W04Tag1HesseFileEinlesenUndAuswerten {
         System.out.println("1.: Anzahl der Zeichen: " + textLength(text));
         System.out.println("2.: Anzahl Charaktere: " + at.dcv.w03.W03Tag2SehrlangerString.textLengthStrip(text));
         System.out.print("3.: Anzahl Worte: " + at.dcv.w03.W03Tag2SehrlangerString.wordCount(text));
-        System.out.println(" 3a.: Clemens Anzahl Wort: " + at.dcv.w03.W03Tag2SehrlangerString.wordCountClemens(text));
+        //System.out.println(" 3a.: Clemens Anzahl Wort: " + at.dcv.w03.W03Tag2SehrlangerString.wordCountClemens(text));
         System.out.print("4.: Längstes Wort: " + at.dcv.w03.W03Tag2SehrlangerString.longestString(text));
         System.out.println(" 4a .: Kürzestes Wort: " + at.dcv.w03.W03Tag2SehrlangerString.shortestString(text));
         System.out.println("5.: erstes Wort alphabetisch: " + at.dcv.w03.W03Tag2SehrlangerString.firstWord(text));
         System.out.println("6.: Anzahl Hesse: " + at.dcv.w03.W03Tag2SehrlangerString.wordCountHesse(text));
         System.out.println("7.: LowerCaseCount: " + at.dcv.w03.W03Tag2SehrlangerString.lowerCaseCount(text));
         System.out.println("8.: UpperCaseCount: " + at.dcv.w03.W03Tag2SehrlangerString.upperCaseCount(text));
+        at.dcv.w03.W03Tag2SehrlangerString.printWordSort(text);
     }
 
     //         1.: Anzahl der Charactere
