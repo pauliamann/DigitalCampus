8ba8517f023dcd544bd52029429ba8fff89987c8
paul <paul.amann@gmx.at>
2020-10-06 09:31:44 +0200

    Check der DateiArgumente zugefuegt

--- DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w03/W03Tag5Scanner.java
+++ DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w03/W03Tag5Scanner.java
@@ -7,40 +7,35 @@ import java.util.Scanner;
 public class W03Tag5Scanner {
 
     public static void main(String[] args) {
+
+        System.out.println( "Argumente beim main(): ");
+        for (int i = 0; i < args.length; ++i) {
+            System.out.println("args[" + i + "]: " + args[i]);
+        }
+
+
+
+        // ---------------------
         Scanner sc = new Scanner(System.in);
 
-        int num1 = 0;
-        try {
-            String input1 = sc.nextLine();
-            num1 = Integer.valueOf(input1);
-        } catch (NumberFormatException nfe) {
-            System.out.println("Wir haben ein Problem.");
+        System.out.println("Gib die erste Zahl ein: ");
+        int num1 = Integer.MIN_VALUE;
+
+        while (num1 == Integer.MIN_VALUE) {
+            try {
+                String input1 = sc.nextLine();
+                int value = Integer.valueOf(input1);
+                if ((value >=1) && (value <=20)) { // Prüfung ob die Zahl im Zahlenraum ist.
+                    num1 = value;
+                } else {
+                    System.out.println("Gib eine Zahl zwischen 1 & 20 ein:");
+                }
+            } catch (NumberFormatException nfe) { // Prüfung ob die Eingabe eine Zahl ist.
+                System.out.println("Sie müssen eine Zahl eingeben:");
+            }
         }
         System.out.println("Gib bitte die zweite Zahl ein: ");
         sc.nextInt();
-
-
-
-
-
-        /*int InputMismatchException = 0-9;
-
-           System.out.println("Was willst du addieren?");
-          try {
-              System.out.println("erste Zahl:");
-              int choiceA = sc.nextInt();
-              System.out.println("zweite Zahl:");
-              int choiceB = sc.nextInt();
-              System.out.println("Ergebnis: " + (choiceA + choiceB));
-          } catch (InputMismatchException ime) {
-              System.out.println("Das is keine Zahl");
-
-          }*/
-
-
-
-
-
 
     }
 }
