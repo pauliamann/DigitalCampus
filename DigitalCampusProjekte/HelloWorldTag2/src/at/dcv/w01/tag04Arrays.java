29db5eda37dd2d821c301ac9a2b9feeb9223187e
paul <paul.amann@gmx.at>
2020-10-06 09:33:54 +0200

    Lesbarkeit verbessert

--- DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w01/tag04Arrays.java
+++ DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w01/tag04Arrays.java
@@ -7,43 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class tag04Arrays {
     public static void main(String[] args) {
 
-/*        int variable = 5;
-        int maxCount;
-        int ergebnis;
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Gib eine Zahl ein:");
-        variable = scan.nextInt();
-
-        System.out.println("Gib eine Maximale Multiplikations Anzahl ein:");
-        maxCount = scan.nextInt();
-
-
-        for (int count=1; count <= maxCount; ++count) {
-            ergebnis = variable * count;
-            System.out.println(variable + " * " + count + " = " + ergebnis);
-        }*/
-
-/*        int variableX;
-        int variableY;
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Gib eine Zahl für X ein:");
-        variableX = scan.nextInt();
-
-        System.out.println("Gib eine Zahl für Y ein:");
-        variableY = scan.nextInt();
-
-        boolean xGreaterY = true;
-
-        xGreaterY = variableX > variableY;
-        System.out.println();
-
-        if (xGreaterY){
-            System.out.println( "X ist größer als Y");
-        } else {
-            System.out.println(" X ist nicht größer als Y");
-        }*/
 
         /* ---
          * ----  Computer gibt eine Rechenaufgabe aus und verlangt nach Ergebnis
