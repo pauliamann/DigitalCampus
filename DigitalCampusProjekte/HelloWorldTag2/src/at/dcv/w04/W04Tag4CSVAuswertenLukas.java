f79c3cfc46f7e5e63f247adbd7fb6c86f738b627
paul <paul.amann@gmx.at>
2020-10-08 11:55:43 +0200

    added: printColumn, splitLines, checkPreconditions

--- DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w04/W04Tag4CSVAuswertenLukas.java
+++ DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w04/W04Tag4CSVAuswertenLukas.java
@@ -1,41 +1,61 @@
 package at.dcv.w04;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class W04Tag4CSVAuswertenLukas {
     public static void main(String[] args) {
         checkPreconditions(args);
 
-        // Einlesen der Datei über buffered reader; (der Buffered Reader liest so lange bis er das Zeilenende erreicht.) (der Filereader liest Zeichen für Zeichen)
-        // Kann Zeile für Zeile lesen.
-        BufferedReader fis = null;
-        try {
-            // BufferedReader -> Lesen von Zeilen  mithilfe eines anderen Readers
-            // Filereader -> Lesen von Zeichen aus Datei
-            fis = new BufferedReader(new FileReader(args[0]));
-            String line = null;
-            String text = "";
+        String text = readfile(args[0]);
 
-            // line wird der Wert aus fis.readline() zugewiesen.
-            // Die komplette Zuweisung (line = fis.readline()) hat den Wert der zugewiesen wurde.
-            // Deswegen können wir auf null prüfen.
-            while ((line = fis.readLine()) != null) {
-                text += line;  //hier wird der ausgelesene Text in einen String gespeichert. Nach jeder line wird ein "\n" zugefügt.
-                text += "\n";
-            }
+        if (text == null) {
+            System.out.println("Text konnte nicht eingelesen werden.");
+            System.exit(1);
+        }
 
-        } catch (IOException exc) {
-            exc.printStackTrace();
-        } finally { // Hier sagen wir dass das Programm beendet werden soll.
-            // finally wird immer ausgeführt.
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException exc) {
-                    //noop
-                }
+        String[] lineArray = splitLines(text);
+
+
+        for (String line: lineArray) {
+            String[] columns = line.split(",");
+            for (String column: columns) {
+                printColumn(column, 10);
+                System.out.print(" ");
             }
+            System.out.println();
+        }
+    }
+
+    public static void printColumn (String column, int length) {
+        if (column.length() < length) {
+            // Auffüllen mit Leerzeichen
+            int spaces = length - column.length();
+            for (int i = 0; i < spaces; i++) {
+                System.out.print(" ");
+
+            }
+            System.out.print(column);
+        } else if (column.length() > length){
+            // String muss abgeschnitten werden
+            System.out.print(column.substring(0, length));;
+        } else {
+            // Wenn gleich lang: einfach ausgeben
+            System.out.print(column);
+        }
+
+    }
+
+
+    public static void printLines (String[] lineArray) {
+        /*        // for Zählschleife mit index (als alternative Ausgabe verwenden wir die for each Schleife)
+        for (int i = 0; i < lineArray.length; i++) {
+            System.out.println(lineArray[i]);
+        }
+        System.out.println("---");*/
+
+        // for each Schleife
+        for (String line: lineArray) {
+            System.out.println(line);
         }
     }
 
@@ -58,6 +78,53 @@ public class W04Tag4CSVAuswertenLukas {
             System.out.println("Die Datei existiert nicht... ");
             System.exit(1);
         }
+    }
+
+    public static String readfile (String filename) {
+        // Einlesen der Datei über buffered reader; (der Buffered Reader liest so lange bis er das Zeilenende erreicht.) (der Filereader liest Zeichen für Zeichen)
+        // Kann Zeile für Zeile lesen.
+        BufferedReader fis = null;
+        try {
+            // BufferedReader -> Lesen von Zeilen  mithilfe eines anderen Readers
+            // Filereader -> Lesen von Zeichen aus Datei
+            fis = new BufferedReader(new FileReader(filename));
+            String line = null;
+            String text = "";
+
+            // line wird der Wert aus fis.readline() zugewiesen.
+            // Die komplette Zuweisung (line = fis.readline()) hat den Wert der zugewiesen wurde.
+            // Deswegen können wir auf null prüfen.
+            while ((line = fis.readLine()) != null) {
+                text += line;  //hier wird der ausgelesene Text in einen String gespeichert. Nach jeder line wird ein "\n" zugefügt.
+                text += "\n";
+            }
+            // Wir ersetzen alle " durch nichts.
+            //
+            return text.replaceAll("\"", "");
+        } catch (IOException exc) {
+            exc.printStackTrace();
+            //
+            // Bei Fehlerfall wird null zurückgegeben.
+            return null;
+        } finally {
+            // Hier sagen wir dass das Programm beendet werden soll.
+            // finally wird immer ausgeführt.
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException exc) {
+                    //noop (no operation)
+                }
+            }
+        }
+    }
+
+    public static String[] splitLines(String text) {
+        // Text wird in ein Array gesplittet
+        String[]  lineArray = text.split("\n");
+
+        return lineArray;
+
     }
 
 }
