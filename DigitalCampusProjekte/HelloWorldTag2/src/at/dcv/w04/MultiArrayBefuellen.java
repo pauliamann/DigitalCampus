5f0a0efc057dd4778529c1a1e4867cd762928800
paul <paul.amann@gmx.at>
2020-10-12 09:06:52 +0200

    2 Dimensionales Array mit CSV Daten befüllen und auswerten

---
+++ DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w04/MultiArrayBefuellen.java
@@ -0,0 +1,113 @@
+package at.dcv.w04;
+
+
+import java.io.*;
+
+public class MultiArrayBefuellen {
+    public static void main(String[] args) {
+        String[][] table = readCSV("C:\\Users\\DCV\\Desktop\\TableData.csv");
+        cleanData(table);
+
+        countAvarage(table, 5);
+        printDynTable(table);
+    }
+
+    public static void cleanData(String[][] table){
+        for (int i = 0; i  < table.length; ++i) {
+            for (int j = 0; j < table[i].length; ++j) {
+                table[i][j] = table[i][j]
+                        .replaceAll("^\"", "")
+                        .replaceAll("\"$", "");
+            }
+        }
+    }
+
+    public static int[] calculateColumnWidth(String[][] table){
+        int[] tableWidth = new int[table[0].length];
+        for (int i = 0; i < table.length; ++i){
+            for (int j = 0; j < table[i].length; ++j){
+                if (table[i][j].length() > tableWidth[j]){
+                    tableWidth[j] = table[i][j].length();
+                }
+            }
+        }
+        return tableWidth;
+    }
+
+    public static void printDynTable(String[][] table){
+        int[] tableWidth = calculateColumnWidth(table);
+        for (int i = 0; i < table.length; ++i) {
+            for (int j = 0; j < table[i].length; ++j) {
+                System.out.printf("%"+tableWidth[j]+"s ", table[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void countAvarage(String[][] table, int column){
+        double sum = 0.0;
+        int countSums = 0;
+        for (int i = 1; i < table.length; ++i){
+            double value = Double.MIN_VALUE;
+            try {
+                value = Double.valueOf(table[i][column]);
+            } catch (NumberFormatException nfe){
+                // The field contains an alphanumerical value, we've to ignore it
+            }
+            if (value != Double.MIN_VALUE){
+                sum += value;
+                countSums += 1;
+            }
+        }
+
+
+        System.out.print("The avarage of column " + table[0][column] + " is " + String.format("%.2f", sum / countSums));
+        if (countSums != table.length-1){
+            System.out.print(" (Attention the column contains alphanumerical values. These have been ignored.)");
+        }
+        System.out.println();
+    }
+
+    public static String[][] readCSV(String fileName) {
+        int countLines = countLines(fileName);
+        String[][] table = new String[countLines][];
+
+        BufferedReader br = null;
+        int idxLine = 0;
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            if (br != null) {
+
+                while (br.ready()) {
+                    String line = br.readLine();
+                    table[idxLine] = line.split(",");
+                    ++idxLine;
+                }
+                br.close();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return table;
+    }
+
+    public static int countLines(String fileName) {
+        BufferedReader br = null;
+        int count = 0;
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            if (br != null) {
+
+                while (br.ready()) {
+                    String line = br.readLine();
+                    ++count;
+                }
+                br.close();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return count;
+    }
+}
