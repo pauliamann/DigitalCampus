7bca7535972a263be8056a133dd1c10952490e98
paul <paul.amann@gmx.at>
2020-10-13 09:07:45 +0200

    Hausübung Aufgaben für Homes.csv Analyse

---
+++ DigitalCampusProjekte/HelloWorldTag2/src/at/dcv/w04/Hausuebung.java
@@ -0,0 +1,26 @@
+package at.dcv.w04;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Arrays;
+
+public class Hausuebung {
+
+    /* --- Aufgabenstellung
+    * 1. Tabellarische Darstellung (jede Spalte ist 15 Charaktere breit)
+            Längere Texte werden abgeschnitten.
+            Zwischen den Spalten müssen "" kommen.
+    * 2. Tabellarische Darstellung (Dynamische Spaltenbreite)
+    * 3. Analyse
+    *   a. Wieviel war der Gesamtumsatz
+    *   b. Wieviel Rabatt haben die Kunden bekommen
+    *       (Liste welches Objekt wieviel Rabatt erhalten hat &
+    *        Zusammen im Durchschnitt in Prozent?)
+    *   c. Teuerstes Objekt auf der Liste?
+    *   d. Teuerstes Objekt pro Quadratmeter?
+    *   e. Durchschnittliches Alter der Häuser ?
+    *
+    * */
+
+
+}
